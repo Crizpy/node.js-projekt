00f491f21fafc17ef91904181b6fa1a80fc284fe
christophponath <christoph.ponath@conrad.de>
2018-03-28 15:32:49 +0200

    Working on better session management

---
+++ databaseEntities/session/sessionEntity.js
@@ -0,0 +1,20 @@
+class Session {
+
+    constructor() {
+        this.id = "21mon1"
+        this.userkey = "ianbibib2i"
+
+        this.lastActivity = new Date();
+
+        this.steam = {}
+        this.steam.name = "Peter"
+        this.steam.avatar = "https://google.com/avatar"
+        this.steam.id = "7689383859275725"
+        this.steam.profile = "https://steamcommunity.com/id/crzzpy"
+    }
+
+    set
+
+
+
+}
---
+++ databaseEntities/session/sessionService.js
@@ -0,0 +1,13 @@
+var config = require("../../configuration/default")
+var mysql = require("mysql");
+
+const connection = mysql.createConnection(config.database.connection);
+
+
+connection.query("SELECT * FROM session",[],function (err,res) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log(res)
+    }
+})
